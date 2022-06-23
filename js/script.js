2632737d666f143ca884e30ca95c8a9d942f8f26
Nick Davidson <nick.davidson@gmail.com>
2022-06-23 11:06:10 -0400

    Modify code to group cells into row divs.
    Add style to cells.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
 const container = document.querySelector('#container');
 
-const NUM_ROWS = 16;
-const NUM_COLS = 16;
+const NUM_ROWS = 48;
+const NUM_COLS = NUM_ROWS;
 
 for(let i=0; i<NUM_ROWS; i++){
+    let row = document.createElement('div');
+    row.className = 'row';
+
     for(let j=0; j<NUM_COLS; j++){
         let cell = document.createElement('div');
         cell.id = `cell-${i+1}-${j+1}`;
-        cell.style.width = '16px';
-        cell.style.height = '16px';
-        cell.style.border = `1px solid black`;
-        container.appendChild(cell);
+        cell.className = 'cell';
+
+        if(i===0){
+            cell.style.borderTop = '1px solid black';
+        }
+
+        if(j===0){
+            cell.style.borderLeft = '1px solid black';
+        }
+
+        row.appendChild(cell);
     }
+    
+    container.appendChild(row);
 }

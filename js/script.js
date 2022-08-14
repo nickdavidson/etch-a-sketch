23a3be57fadf7e44bb2b7167fd18b309308f6400
Nick Davidson <nick.davidson@gmail.com>
2022-08-14 03:47:10 -0400

    Add button to toggle shading mode.
    Add button to toggle random color mode.
    
    Add button to toggle eraser mode.
    
    Add new optionPanel div for new buttons.
    
    Move shading mode functionality to its own function alphaFill.

--- js/script.js
+++ js/script.js
@@ -15,6 +15,24 @@ board.id = "board";
 const inputPanel = document.createElement("div");
 inputPanel.id = "input-panel";
 
+const optionPanel = document.createElement("div");
+optionPanel.id = "option-panel";
+
+const shaderButton = document.createElement("button");
+shaderButton.innerHTML = "Shading Mode OFF"
+shaderButton.id = "shader-button";
+shaderButton.addEventListener('click', shadingToggle);
+
+const randomRGBButton = document.createElement("button");
+randomRGBButton.innerHTML = "Rainbow Mode OFF"
+randomRGBButton.id = "rainbow-button";
+randomRGBButton.addEventListener('click', randomRGBToggle);
+
+const eraserButton = document.createElement("button");
+eraserButton.innerHTML = "Eraser OFF"
+eraserButton.id = "eraser-button";
+eraserButton.addEventListener('click', eraserToggle);
+
 const newBoardButton = document.createElement("button");
 newBoardButton.innerHTML = "New Board";
 newBoardButton.id = "new-board";
@@ -39,6 +57,10 @@ sliderOutput.innerHTML = `${gridSlider.value}x${gridSlider.value}`;
 sliderContainer.appendChild(gridSlider);
 sliderContainer.appendChild(sliderOutput);
 
+optionPanel.appendChild(shaderButton);
+optionPanel.appendChild(randomRGBButton);
+optionPanel.appendChild(eraserButton);
+
 inputPanel.appendChild(newBoardButton);
 inputPanel.appendChild(sliderContainer);
 
@@ -48,36 +70,106 @@ gridSlider.oninput = function() {
 
 let cells;
 
+container.appendChild(optionPanel);
 container.appendChild(board);
-container.appendChild(inputPanel);
 drawBoard(BOARD_SIZE);
+board.append(inputPanel);
 
 preventDrag();
 
+
+let shadingMode = false;
+let randomRGBMode = false;
+let eraserMode = false;
+
+function shadingToggle(){
+    if(eraserMode){
+        eraserToggle();
+    }
+
+    if(!shadingMode){
+        shaderButton.innerHTML = "Shading Mode ON";
+        shadingMode = true;
+    } else {
+        shaderButton.innerHTML = "Shading Mode OFF";
+        shadingMode = false;
+    }
+}
+
+function randomRGBToggle(){
+    if(eraserMode){
+        eraserToggle();
+    }
+
+    if(!randomRGBMode){
+        randomRGBButton.innerHTML = "Rainbow Mode ON";
+        randomRGBMode = true;
+    } else {
+        randomRGBButton.innerHTML = "Rainbow Mode OFF";
+        randomRGBMode = false;
+    }
+}
+
+function eraserToggle(){
+    if(shadingMode){
+        shadingToggle();
+    }
+    if(randomRGBMode){
+        randomRGBToggle();
+    }
+    
+    if(!eraserMode){
+        eraserMode = true;
+        eraserButton.innerHTML = "Eraser ON";
+    } else {
+        eraserMode = false;
+        eraserButton.innerHTML = "Eraser OFF";
+    }
+
+}
 
 function cellFill(e){
     if((e.type==='mouseover') && !isMouseDown){
         return;
     }
+
     let red = 0;
     let green = 0;
     let blue = 0;
+    let alpha = 1;
 
+    if(eraserMode){
+        red = 255;
+        green = 255;
+        blue = 255;
+        this.setAttribute('alpha', 0);
+    }
 
-    let random = false;
-
-    if(random){
+    if(randomRGBMode){
         red = Math.floor(Math.random()*255);
         green = Math.floor(Math.random()*255);
         blue = Math.floor(Math.random()*255);
     }
 
-    let alpha = this.getAttribute("alpha")*1;
+    if(shadingMode){
+        alphaFill(this, red, green, blue);
+    }
+    else{
+        if(!eraserMode){
+            this.setAttribute('alpha', alpha);
+        }
+        this.style.backgroundColor = `rgba(${red},${green},${blue}, ${alpha})`;
+    }
+}
+
+function alphaFill(cell, r, g, b){
+    let alpha = cell.getAttribute("alpha")*1;
+    console.log(alpha);
     if(alpha<1){
         alpha+=0.10;
     }
-    this.setAttribute(`alpha`, `${alpha}`);
-    this.style.backgroundColor = `rgba(${red},${green},${blue},${alpha})`;
+    cell.setAttribute(`alpha`, `${alpha}`);
+    cell.style.backgroundColor = `rgba(${r},${g},${b},${alpha})`;
 }
 
 function preventDrag(){
@@ -134,7 +226,7 @@ function drawBoard(size){
         board.appendChild(row);
     }
 
-    container.style.width = `${size*CELL_SIZE}px`;
+    //container.style.width = `${size*CELL_SIZE}px`;
     updateCells();
 }
 
